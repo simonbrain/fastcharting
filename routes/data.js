6970cb576eff85e3fbf6cd371a88daa06f44cfed
simonbrain <simon.brain@outlook.com>
2021-05-09 13:04:32 +0100

    Auto Dev Push

--- routes/data.js
+++ routes/data.js
@@ -12,24 +12,29 @@ router
     })
 
 router
-    .get("/lse",(req,res) => {
-            const getLonData = async() => {
-                try {
-                    const response = await axios.get(
+.get("/test/:id",(req,res) => {
+    res.send('Param sent was: '+ req.params.id)
+})
+
+//router
+    //.get("/lse",(req,res) => {
+           // const getLonData = async() => {
+                //try {
+                    //const response = await axios.get(
                        // 'http://storage.cloud.google.com/charting-lon-bucket/lse/test_lse.json?authuser=2'
-                       'http://storage.cloud.google.com/charting-lon-bucket/lse/test_lse.json'
-                    )
-                    res.send(response.data)
-                    console.log(response)
-                }
-                catch (error){
-                    console.log(error)
-                }
-            }
-            getLonData()
+                       //'http://storage.cloud.google.com/charting-lon-bucket/lse/test_lse.json'
+                    //)
+                    //res.send(response.data)
+                    //console.log(response)
+                //}
+                //catch (error){
+                    //console.log(error)
+                //}
+            //}
+            //getLonData()
         //res.send('This is the lse directory')
         //res.send()
-    })
+    //})
 
 
 module.exports = router;
